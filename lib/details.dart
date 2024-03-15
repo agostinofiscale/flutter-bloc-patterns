ba08861c78a58bdf1c70b81556261bc6a16f4bc7
Agostino Fiscale <agostinofiscale@gmail.com>
2024-03-16 00:54:21 +0100

    Change: export details events

--- lib/details.dart
+++ lib/details.dart
@@ -2,4 +2,5 @@
 library details;
 
 export 'package:flutter_bloc_patterns/src/details/details_bloc.dart';
+export 'package:flutter_bloc_patterns/src/details/details_events.dart';
 export 'package:flutter_bloc_patterns/src/details/details_repository.dart';
